e364321b354f8f691360c26f97167c9c8400570f
Bartosz Kozlowski <kozlowski.bartosz@protonmail.com>
2024-12-08 17:27:16 +0100

    Further flask restructurization

---
+++ frontend/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from frontend.routes import main_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    app.register_blueprint(main_bp)
+
+    return app
