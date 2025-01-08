94817d907b50eb5ebfc7e947b3d860c168b2c7ea
Bartosz Kozlowski <kozlowski.bartosz@protonmail.com>
2025-01-08 22:19:23 +0100

    Fix error when viewing results without a parsed file

--- backend/pcap_reader.py
+++ backend/pcap_reader.py
@@ -3,9 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import io
 
+
 def read_packets(pcap_packets):
-    packets = rdpcap(pcap_packets)
     packet_data = []
+    try:
+        packets = rdpcap(pcap_packets)
+    except AttributeError:
+        return packet_data
+
     for packet in packets:
         packet_info = {
             "source": packet.sprintf("{IP:%IP.src%}"),
@@ -15,15 +20,20 @@ def read_packets(pcap_packets):
             "details": str(packet.show(dump=True)),
         }
         packet_data.append(packet_info)
+
     return packet_data
 
 # Export packets to pandas dataframe
+
+
 def packets_to_df(pcap_packets):
     data = read_packets(pcap_packets)
     df = pd.DataFrame(data)
     return df
 
 # Plot pie chart and generate png
+
+
 def plot_pie_png(df, column):
     value_counts = df[column].value_counts()
 
@@ -40,7 +50,8 @@ def plot_pie_png(df, column):
 
     # Plot as a pie chart
     fig, ax = plt.subplots(figsize=(6, 6))
-    grouped_counts.plot.pie(autopct='%1.1f%%', ax=ax, title="Distribution of Values")
+    grouped_counts.plot.pie(autopct='%1.1f%%', ax=ax,
+                            title="Distribution of Values")
     ax.set_ylabel('')  # Hide the y-axis label
 
     # Save the plot to a BytesIO object
