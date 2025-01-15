7a6854601ba414ceeb4be613b78e3fc618cfcd42
Chadzyn <mateusz.chadzynski@gmail.com>
2025-01-15 22:05:03 +0100

    Increase max size of saved files

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     UPLOAD_FOLDER = 'frontend/static/uploads'
     DEBUG = True
+    MEGABYTE = (2 ** 10) ** 2
+    MAX_CONTENT_LENGTH = None
+    MAX_FORM_MEMORY_SIZE = 50 * MEGABYTE # Set max file size for saving
